189a6f03dfbc50610319331ba91a0b4617b42cb0
Aaron <aakiswa@gmail.com>
2024-04-22 16:45:34 +0800

    Add events in scipt file

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
-let player = 'ROcK';
-player = player.toLowerCase();
-let computer = undefined;
-let pScore = 0;
-let cScore = 0;
+let playerChoice = '';
+// playerChoice = playerChoice.toLowerCase();
+let computerChoice = undefined;
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
 
 const getComputerChoice = () => {
-      computer = Math.floor(Math.random()*3)+1;
-      if(computer === 1){
+      computerChoice = Math.floor(Math.random() * 3) + 1;
+      if(computerChoice === 1){
             return "rock";
-      }else if (computer === 2){
+      }else if (computerChoice === 2){
             return "paper";
-      }else if (computer === 3){
+      }else if (computerChoice === 3){
             return "scissors";
-      }else{
-            console.log("Error: Number that is generated is more than 3")
       }
 }
 
-const playRound = (playerSelection, computerSelection) => {
+const startRound = (playerSelection, computerSelection) => {
       if(playerSelection === computerSelection){
-            return;
+            return drawScore++;
       }else if(
             (playerSelection === "rock" && computerSelection === "scissors") ||
             (playerSelection === "paper" && computerSelection === "rock") ||
             (playerSelection === "scissors" && computerSelection === "paper")
       ){
-            return pScore++;
+            return playerScore++;
       }else {
-            return cScore++;
+            return computerScore++;
       }
 }
 
-const playGame = () => {
-      for(let i = 1; i <= 5; i++){
-            playRound(player, getComputerChoice());
-      }
-      if(pScore > cScore){
-            return console.log("Player Wins");
-      }else if(cScore > pScore){
-            return console.log("Computer Wins");
-      }else{
-            return console.log("Draw");
-      }
-}
+// const playGame = () => {
+//       // for(let i = 1; i <= 5; i++){
+//       //       startRound(playerChoice, getComputerChoice());
+//       // }
+//       startRound(playerChoice, getComputerChoice());
+//       if(playerScore > computerScore){
+//             return playerScore++;
+//       }else if(computerScore > playerScore){
+//             return computerScore++;
+//       }else{
+//             return console.log("Draw");
+//       }
+// }
 
-playGame();
-console.log(pScore, cScore);
+const rpsContainer = document.querySelector('.main-rps-cont');
+rpsContainer.addEventListener('click', (event) => {
+      let target = event.target;
+      console.log(event.target);
+
+      switch(target.id) {
+            case 'rock': 
+                  playerChoice = 'rock';
+                  break;
+            case 'paper': 
+                  playerChoice = 'paper';
+                  break;
+            case 'scissors': 
+                  playerChoice = 'scissors';
+                  break;
+      }
+      
+      startRound(playerChoice, getComputerChoice());
+
+      const result = document.querySelector(".results");
+      result.textContent = `Player: ${playerScore}
+                            Computer: ${computerScore}
+                            Draw: ${drawScore}`;
+});
+
+
+
+
 
